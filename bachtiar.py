f1a4e420936059e48f1ae003fd4e7cc0d93472e3
lanacarkecor <145952175+lanacarkecor@users.noreply.github.com>
2023-09-25 10:52:14 +0700

    bachtiar.py

--- bachtiar.py
+++ bachtiar.py
@@ -2,7 +2,7 @@ apple_price = 2
 #Berikan 10 ke variable money
 money=10
 
-input_count = input ('Mau berapa apol?: ")
+input_count = input ('Mau berapa apol?: ')
 count = int (input_count)    
 total price apple_price* count
 
@@ -13,9 +13,11 @@ print('Harga total adalah ' + str(total price) + ' dolar')
 if(total price<money): 
 print('Anda telah membeli ' +str(input_count)+ ' apel')
 print('Uang Anda tinggal ' +str(total price-money)+ ' dolar' )
+
 elif (total prices==money):
 print('Anda telah membeli ' +str(input_count)+ ' apel')
 print('Dompet Anda kosong')
+
 else:
 print('Ulang Anda tidak mencukupi')
 print('Anda tidak dapat membeli apel sebanyak itu')
